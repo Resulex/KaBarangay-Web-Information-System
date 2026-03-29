c60a021c48d0bf8f83c904e709b5c2b890ce9e89
Alex Resurreccion <alexresu@Alexs-MacBook-Air.local>
2026-03-29 18:37:34 +0800

    Fixed and Final push for TA

--- api/announcements/crud.js
+++ api/announcements/crud.js
@@ -2,9 +2,7 @@ import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-const uri = "mongodb+srv://dev-user1:NsDXtHtpEsAGNUcd@kabarangay-system-db.qkeqxvv.mongodb.net/?appName=Kabarangay-system-db";
-
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 const DB_NAME = process.env.DB_NAME;
 const COLLECTION_NAME = process.env.ANNOUNCEMENT_COLLECTION;
